9484a070635da3f4cc7d43ae31154b4bbb8373e2
ciscoLegrand <cisco.glez@gmail.com>
2023-11-01 14:41:01 +0100

    install figaro and first configurations

--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,20 @@ Bundler.require(*Rails.groups)
 module Gramoladisco
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults Rails::VERSION::STRING.to_f
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
+    config.generators do |gen|
+      gen.assets            false
+      gen.helper            false
+      gen.test_framework    :minitest
+      gen.jbuilder          false
+      gen.orm               :active_record, primary_key_type: :uuid
+      gen.system_tests      false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -23,5 +30,9 @@ module Gramoladisco
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Europe/Madrid'
+    config.i18n.available_locales = %i[es en]
+    config.i18n.default_locale = :es
+    config.i18n.fallbacks = true
   end
 end
