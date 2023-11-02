a70489032d289992f87c8fc5453ded688b4a645e
ciscoLegrand <cisco.glez@gmail.com>
2023-11-02 07:34:39 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
+  scope module: 'frontend' do
+    root 'pages#index'
+    resources :albums do
+      get :verify_password, on: :member
+      post :verify_password, on: :member
+      collection do
+        post :index
+      end
+    end
+  end
+  namespace :admin do
+    root 'dashboard#index'
+    resources :albums do
+      patch :publish, on: :member
+      post  :search, on: :collection
+    end
+    resources :images, only: %i[create]
+    delete 'images/:album_id/delete-all', to: 'images#delete_all', as: :delete_all
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
