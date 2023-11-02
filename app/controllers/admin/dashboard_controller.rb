dba7ba76864873fd38d2d069996959a4a192b935
ciscoLegrand <cisco.glez@gmail.com>
2023-11-02 07:26:32 +0100

    create admin dashboard albums and images controllers

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,13 @@
+class Admin::DashboardController < ApplicationController
+  # before_action :authenticate_user!
+  before_action :ensure_admin
+  layout 'admin'
+  def index; end
+
+  private
+  def ensure_admin
+    # unless current_user.admin?
+    #   redirect_to root_path, alert: t('admin.dashboard.ensure_admin.alert')
+    # end
+  end
+end
