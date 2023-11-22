8312ede4f39aa6e55a93efb1815972def5abc356
ciscoLegrand <cisco.glez@gmail.com>
2023-11-22 23:49:34 +0100

    add trix

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -6,43 +6,43 @@ class ContactsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get contacts_url
+    get admin_contacts_url
     assert_response :success
   end
 
   test "should get new" do
-    get new_contact_url
+    get new_admin_contact_url
     assert_response :success
   end
 
   test "should create contact" do
     assert_difference("Contact.count") do
-      post contacts_url, params: { contact: {  } }
+      post admin_contacts_url, params: { contact: {  } }
     end
 
     assert_redirected_to contact_url(Contact.last)
   end
 
   test "should show contact" do
-    get contact_url(@contact)
+    get admin_contact_url(@contact)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_contact_url(@contact)
+    get edit_admin_contact_url(@contact)
     assert_response :success
   end
 
   test "should update contact" do
-    patch contact_url(@contact), params: { contact: {  } }
+    patch admin_contact_url(@contact), params: { contact: {  } }
     assert_redirected_to contact_url(@contact)
   end
 
   test "should destroy contact" do
     assert_difference("Contact.count", -1) do
-      delete contact_url(@contact)
+      delete admin_contact_url(@contact)
     end
 
-    assert_redirected_to contacts_url
+    assert_redirected_to admin_contacts_url
   end
 end
