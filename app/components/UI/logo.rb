880936b83e52b5f227e685c0a9adc8bb2faa0f03
ciscoLegrand <cisco.glez@gmail.com>
2023-11-23 18:00:55 +0100

    Refactor UI components with content tag helpers and delete html files

--- app/components/UI/logo.rb
+++ app/components/UI/logo.rb
@@ -4,6 +4,12 @@ class UI::Logo < ViewComponent::Base
     @logo = logo
   end
 
+  def call
+    content_tag(:div, class: 'w-full h-16 flex justify-center items-center content-center') do
+      link_to(logo_tag, @link, class: 'flex items-center justify-center my-3')
+    end
+  end
+
   def logo_tag
     if svg?(@logo)
       inline_svg_tag(@logo, class: 'w-12 h-12')
--- app/components/UI/nav_link.rb
+++ app/components/UI/nav_link.rb
@@ -5,7 +5,23 @@ class UI::NavLink < ViewComponent::Base
     @title = title
   end
 
-  def active_link_to(text= nil, path = nil, **options, &block)
+  def call
+    active_link_to(@path, class: 'nav-link w-full flex justify-center items-center py-2 text-center') do
+      safe_join([title_content, icon_content])
+    end
+  end
+
+  private
+
+  def title_content
+    content_tag(:span, @title, class: 'w-full text-lg', data: { 'decrease-target': 'link' })
+  end
+
+  def icon_content
+    inline_svg_tag(@icon, class: 'w-8 h-8', data: { 'decrease-target': 'iconLeft' })
+  end
+
+  def active_link_to(text = nil, path = nil, **options, &block)
     link = block_given? ? text : path
 
     options[:class] = class_names(options[:class], 'nav-link-active') if current_page?(link)
