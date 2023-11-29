3c73d95150059efd4f27e9f87f30f55ca1f10151
ciscoLegrand <cisco.glez@gmail.com>
2023-11-29 22:23:06 +0100

    Add order_by and filter_by_text scopes to Contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,7 @@ class Contact < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :subject, length: { minimum: 10, maximum: 500 }
+
+  scope :order_by, ->(column, direction) { order("#{column} #{direction}") }
+  scope :filter_by_text, ->(text) { where('title ILIKE ? OR email ILIKE ?', "%#{text}%", "%#{text}%") }
 end
