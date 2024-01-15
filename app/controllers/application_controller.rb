f53bd7326d84f3ac8ee7c5e6c23da8a755c481d6
cisco gonzalez <cisco.gonzalez@blipol.es>
2024-01-15 22:17:56 +0100

    Add user authentication and permitted parameters for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   include Logging
   add_flash_types :success, :error, :alert, :notice
+
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname, :phone_number, :role, :email, :password, :password_confirmation])
+  end
 end
