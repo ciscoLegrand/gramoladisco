472b2d94092ce64ca34085d0ca68802aeb59f8aa
cisco gonzalez <cisco.gonzalez@blipol.es>
2024-02-09 10:22:28 +0100

    Handle google authentication on omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,40 +1,22 @@
 # frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
+  include Logging
 
   def google_oauth2
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    handle_auth "Google"
+  end
 
+  private
+
+  def handle_auth(kind)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    info_log(@user.inspect)
     if @user.persisted?
-      sign_in_and_redirect @user
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
+      sign_in_and_redirect @user, event: :authentication
     else
-      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
+      session['devise.auth_data'] = request.env['omniauth.auth'].except('extra')
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 end
