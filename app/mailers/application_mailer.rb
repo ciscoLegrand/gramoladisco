ce0d98430b444eadc61296d770da29b94bdb52fe
cisco gonzalez <cisco.gonzalez@blipol.es>
2024-02-12 21:41:38 +0100

    Update mailer configuration ⛑️

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "fran@lagramoladisco.com"
   layout "mailer"
-  prepend_view_path "app/views/mailers"
+  prepend_view_path Rails.root.join('app', 'views', 'mailers')
 end
