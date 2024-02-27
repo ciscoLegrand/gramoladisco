d6564acac4b72153a952c768374522b228367d57
cisco gonzalez <cisco.gonzalez@blipol.es>
2024-02-27 22:10:02 +0100

    HOTFIX credentials

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,9 @@
-Aws.config.update({
-  region: 'eu-central-1'
-})
+require 'aws-sdk-s3'
+
+$spaces = Aws::S3::Client.new(
+  region: Rails.application.credentials.dig(:DO_REGION),
+  endpoint: Rails.application.credentials.dig(:DO_ENDPOINT),
+  access_key_id: Rails.application.credentials.dig(:DO_ACCESS_KEY_ID),
+  secret_access_key: Rails.application.credentials.dig(:DO_SECRET_ACCESS_KEY),
+  force_path_style: true
+)
