5501e8a97afc7ca2032346ebef4cd5548d173e36
cisco gonzalez <cisco.gonzalez@blipol.es>
2024-03-16 17:45:19 +0100

    Fix read more behaviour on home page reviews carroousel

--- app/javascript/controllers/read_more_controller.js
+++ app/javascript/controllers/read_more_controller.js
@@ -1,16 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["content", "button"];
+  static targets = ["content", "button", "wrapper"];
 
   connect() {
-    this.checkTruncation();
+    this.checkTruncation()
+    this.readMoreText = this.buttonTarget.dataset.readMoreText;
+    this.readLessText = this.buttonTarget.dataset.readLessText;
   }
 
   toggle() {
-    this.contentTarget.classList.toggle('max-h-16');
+    this.contentTarget.classList.toggle('max-h-24');
     this.contentTarget.classList.toggle('max-h-none');
-    this.buttonTarget.textContent = this.buttonTarget.textContent.trim() === "Leer más" ? "Leer menos" : "Leer más";
+    this.wrapperTarget.classList.toggle('h-80');
+    this.wrapperTarget.classList.toggle('max-h-80');
+    if (this.buttonTarget.textContent.trim() === this.readMoreText) {
+      this.buttonTarget.textContent = this.readLessText;
+    } else {
+      this.buttonTarget.textContent = this.readMoreText;
+    }
   }
 
   checkTruncation() {
