0e9960ab9cb2e7f043efcfdbaefd438da43d57c3
cisco gonzalez <cisco.gonzalez@blipol.es>
2024-03-21 19:08:44 +0100

    Add staging environment

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "fran@lagramoladisco.com"
+  default from: "fran@lagramoladisco.com" if Rails.env.staging?
+  default from: "cisco.glez@gmail.com" if Rails.env.production? # TODO: cambiar este email.
   layout "mailer"
   prepend_view_path Rails.root.join('app', 'views', 'mailers')
 end
